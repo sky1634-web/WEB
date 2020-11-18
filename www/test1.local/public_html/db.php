e65ad63356ea9882a4ecb3ab9bb90f613201fefa
sky1634-web <74027674+sky1634-web@users.noreply.github.com>
2020-11-18 17:21:58 +0400

    Add files via upload
    Добавил комментарии

--- www/test1.local/public_html/db.php
+++ www/test1.local/public_html/db.php
@@ -5,36 +5,38 @@ R::setup( 'mysql:host=localhost;dbname=test1.local',
 
 session_start();
 function news($new,$ht)
-{
-        if ($_SESSION['logged_user']->id == 1) {
-        if ((mb_strlen($new, 'utf-8')<180) || (voidstr($new)) || (voidstr($ht))) {
-                 echo "Ошибка т.к TEXT < 180 Symbol or text only with void symbols<br>";
+{       if(!(isset($_SESSION))) //проверка на авторизацию(не относится к заданию)
+        if ($_SESSION['logged_user']->id == 1) { //новость может редактировать только пользователь, id которого = 1
+        if ((mb_strlen($new, 'utf-8')<180) || (voidstr($new)) || (voidstr($ht))) { //mb_strlen проверяет на количество символов, voidstr проверяет новость и ссылку на пустоту.
+                 echo "Ошибка т.к TEXT < 180 Symbol or text only with void symbols<br>"; //Вывод ошибок
                  echo '<div style="color: red;">Новость не создана<br/>
                  Можете перейти на <a href="/">главную</a> страницу!</div><hr>';
         }
         else
         {
-        $p=180;
-        $a = mb_strimwidth($new, 0 , $p);
-        point($a);
-        $a=trim($a);
-        $pieces = explode(" ", $a);
-        $cutted = array_slice($pieces, 0, count($pieces)-2);
-        $a = implode(" ",$cutted);
-        echo "$a";
+        $p=180; //переменная для обреза новости
+        $a = mb_strimwidth($new, 0 , $p); //в переменной $a будут содержаться только первые 180 символов переменной $new
+        point($a);//удаляем текст,состоящий из точек(тестовая проверка)
+        $a=trim($a);//удаляет пустые пробелы с начала и конца
+        $pieces = explode(" ", $a);//преобразуем текст в массив слов,разделителем является пробел
+        $cutted = array_slice($pieces, 0, count($pieces)-2);//удаляет из массива 2 поседних символа
+        $a = implode(" ",$cutted);//записываем в $a обрезанную строчку(без 2 последних слов)
+        echo "$a";//далее идет вывод текста на экран(перед добавлением на страницу и в бд)
         echo '<a href="'.$ht.'">'." ".$pieces[count($pieces)-2]." ".$pieces[count($pieces)-1]."...".'</a>';
-        $news = R::dispense('news');
-        $id = R::count('news')+1;
+        $news = R::dispense('news');//запись в бд
+        $id = R::count('news')+1;//id считает количество новостей
         $news->title= "Новость номер "."$id";
         $news->text=$a." ".'<a href="'.$ht.'">'.$pieces[count($pieces)-2]." ".$pieces[count($pieces)-1]."...".'</a>';
         $news->link=$ht;
-        R::store($news);
+        R::store($news);//сохранение в бд
         echo '<div style="color: green;">Новость успешно добавлена!<br/>
             Чтобы её посмотреть <a href="/region">новости</a> региона!</div><hr>';
         }
                                                 }               
 else         echo '<div style="color: red;">Новость не добавлена!Вы не привилегированный пользователь!<br/>
 Вернуться на <a href="/region">главную</a></div><hr>'; 
+else         echo '<div style="color: red;">Новость не добавлена!Вы не авторизованный пользователь!<br/>	
+Вернуться на <a href="/">главную</a></div><hr>'; 
 }
 function voidstr($str) {
 
